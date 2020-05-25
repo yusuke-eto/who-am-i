03e96a87dbc3044fcbeda4f52121b49cfe3659cc
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-05-25 12:43:24 +0900

    👍 mobile対応した

--- src/About.tsx
+++ src/About.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Avatar, Box, Grid, Heading, Paragraph, Button } from "grommet";
+import React, {useContext} from "react";
+import { Avatar, Box, Grid, Heading, Paragraph, Button, ResponsiveContext } from "grommet";
 import { Star } from "grommet-icons";
 import { Element } from "react-scroll";
 import styled from "styled-components";
@@ -11,6 +11,13 @@ const Container = styled.div`
   height: 65vh;
   background-color: #add8e6;
 `;
+
+const MobileContainer = styled.div`
+  height: 130vh;
+  background-color: #add8e6;
+  padding: 0 4vw;
+`;
+
 const IconBox = styled.div`
   margin: 0 auto;
 `;
@@ -29,34 +36,99 @@ const StyledButton = styled(Button)`
   background-color: #f2f2f2;
 `;
 
+const SkillItem = styled.div`
+  border-radius: 5%;
+  background-color: #f2f2f2;
+`
+
 export const About: React.FC = () => {
+  const size = useContext(ResponsiveContext);
   return (
-    <Container>
-      <Box
-        align="center"
-        justify="center"
-        direction="row"
-        pad={{ vertical: "7vh" }}
-      >
-        <Heading color="palevioletred" alignSelf="center">
-          ABOUT ME
-        </Heading>
-      </Box>
-      <Box pad={{ horizontal: "7vw" }}>
-        <Grid
-          rows={["small", "small"]}
-          columns={["small", "large"]}
-          gap="small"
-          // ボックスに含まれおり、中央寄せしたい時は
-          // alignSelf で中央寄せの指定が出来る
-          alignSelf="center"
-          areas={[
-            { name: "icon", start: [0, 0], end: [0, 0] },
-            { name: "paragraph", start: [1, 0], end: [1, 0] },
-            { name: "skills", start: [0, 1], end: [1, 1] },
-          ]}
-        >
-          <Box gridArea="icon">
+    <>
+      { (size != 'small' && size != 'medium' ) &&
+        <Container>
+            <Box
+            align="center"
+            justify="center"
+            direction="row"
+            pad={{ vertical: "7vh" }}
+          >
+            <Heading color="palevioletred" alignSelf="center">
+              ABOUT ME
+            </Heading>
+          </Box>
+        <Box pad={{ horizontal: "7vw" }}>
+          <Grid
+            rows={["small", "small"]}
+            columns={["small", "large"]}
+            gap="small"
+            // ボックスに含まれおり、中央寄せしたい時は
+            // alignSelf で中央寄せの指定が出来る
+            alignSelf="center"
+            areas={[
+              { name: "icon", start: [0, 0], end: [0, 0] },
+              { name: "paragraph", start: [1, 0], end: [1, 0] },
+              { name: "skills", start: [0, 1], end: [1, 1] },
+            ]}
+          >
+            <Box gridArea="icon">
+              <IconBox>
+                <Avatar
+                  size="xlarge"
+                  src="https://ca.slack-edge.com/T02D9RVN1-UAZ6SCS8Z-110785fffc90-512"
+                />
+              </IconBox>
+            </Box>
+            <Box gridArea="paragraph">
+              <StyledParagraph margin="none">
+                福岡のサーバーサイドエンジニア
+                <br />
+                <br />
+                <strong>20016年:</strong>
+                就職活動をする中で、エンジニアリングに興味をもち、テックキャンプに通う
+                <br />
+                <strong>2017年:</strong>
+                福岡の勤怠管理、給与計算を制作するITベンチャーにて、
+                <br />
+                サーバーサイドエンジニアとして働く
+                <br />
+                <strong>2018年:</strong>
+                別のフィンテック系ベンチャーに転職。同じくサーバーサイドエンジニアとして働く
+              </StyledParagraph>
+            </Box>
+            <Box gridArea="skills">
+              <Box justify="center" pad={{ vertical: "5vh" }} direction="row">
+                <StyledStar color="yellow" />
+                <Heading level="3" textAlign="center" margin="none">
+                  SKILL
+                </Heading>
+              </Box>
+              <Box direction="row" justify="between" pad={{ horizontal: "10vw" }}>
+                <StyledButton color="light-2" label="Ruby" size="small" />
+                <StyledButton color="light-2" label="Javascript" size="small" />
+                <StyledButton color="light-2" label="Go" size="small" />
+                <StyledButton color="light-2" label="AWS" size="small" />
+                <StyledButton color="light-2" label="Terraform" size="small" />
+              </Box>
+            </Box>
+          </Grid>
+        </Box>
+        </Container>
+      }
+      {
+        (size == 'small' || size == 'medium' ) &&
+        <MobileContainer>
+          <Box
+            align="center"
+            justify="center"
+            direction="row"
+            pad={{ vertical: "7vh" }}
+          >
+            <Heading color="palevioletred" alignSelf="center">
+              ABOUT ME
+            </Heading>
+          </Box>
+          <Box>
             <IconBox>
               <Avatar
                 size="xlarge"
@@ -64,41 +136,43 @@ export const About: React.FC = () => {
               />
             </IconBox>
           </Box>
-          <Box gridArea="paragraph">
+          <div>
             <StyledParagraph margin="none">
               福岡のサーバーサイドエンジニア
               <br />
               <br />
-              <strong>20016年:</strong>{" "}
+              <strong>20016年</strong>
+              <br />
               就職活動をする中で、エンジニアリングに興味をもち、テックキャンプに通う
               <br />
-              <strong>2017年:</strong>
-              福岡の勤怠管理、給与計算を制作するITベンチャーにて、
+              <strong>2017年</strong>
               <br />
+              福岡の勤怠管理、給与計算を制作するITベンチャーにて、
               サーバーサイドエンジニアとして働く
               <br />
-              <strong>2018年:</strong>
+              <strong>2018年</strong>
+              <br />
               別のフィンテック系ベンチャーに転職。同じくサーバーサイドエンジニアとして働く
             </StyledParagraph>
+          </div>
+          <Box>
+              <Box justify="center" pad={{ vertical: "5vh" }} direction="row">
+                <StyledStar color="yellow" />
+                <Heading level="3" textAlign="center" margin="none">
+                  SKILL
+                </Heading>
+              </Box>
+              <Box direction="row" justify="between" pad={{ horizontal: "10vw" }}>
+                <SkillItem>Ruby</SkillItem>
+                <SkillItem>Javascript</SkillItem>
+                <SkillItem>Go</SkillItem>
+                <SkillItem>AWS</SkillItem>
+                <SkillItem>Terraform</SkillItem>
+              </Box>
           </Box>
-          <Box gridArea="skills">
-            <Box justify="center" pad={{ vertical: "5vh" }} direction="row">
-              <StyledStar color="yellow" />
-              <Heading level="3" textAlign="center" margin="none">
-                SKILL
-              </Heading>
-            </Box>
-            <Box direction="row" justify="between" pad={{ horizontal: "10vw" }}>
-              <StyledButton color="light-2" label="Ruby" size="small" />
-              <StyledButton color="light-2" label="Javascript" size="small" />
-              <StyledButton color="light-2" label="Go" size="small" />
-              <StyledButton color="light-2" label="AWS" size="small" />
-              <StyledButton color="light-2" label="Terraform" size="small" />
-            </Box>
-          </Box>
-        </Grid>
-      </Box>
+        </MobileContainer>
+      }
       {/* </Element> */}
-    </Container>
+    </>
   );
 };
--- src/Feature.tsx
+++ src/Feature.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Heading, Box, Grid, Text, Paragraph } from "grommet";
+import React, { useContext } from "react";
+import { Heading, Box, Grid, Text, Paragraph, ResponsiveContext } from "grommet";
 import { Star } from "grommet-icons";
 import { GiDiscussion, GiStairsGoal } from "react-icons/gi";
 import styled from "styled-components";
@@ -10,67 +10,134 @@ const Container = styled(Box)`
   padding: 0 0 5vh 0;
 `;
 
+const MobileContainer = styled(Box)`
+  width: 100vw;
+  padding: 0 0 5vh 0;
+`
+
 const StyledStar = styled(Star)`
   align-self: center;
   padding: 0 1vw;
 `;
 
 export const Feature: React.FC = () => {
+  const size = useContext(ResponsiveContext);
   return (
-    <Container background="lightblue">
-      <Box direction="row" justify="center" pad={{ top: "5vh" }}>
-        <StyledStar color="yellow" />
-        <Heading level="3" textAlign="center" margin="none">
-          STRENGTH
-        </Heading>
-      </Box>
-      <Box alignSelf="center">
-        <Grid
-          rows={["medium"]}
-          columns={["medium", "medium"]}
-          gap="medium"
-          // @note ここの padding 指定は top だけでもいいかも
-          pad={{ vertical: "5vh" }}
-          areas={[
-            { name: "left", start: [0, 0], end: [0, 0] },
-            { name: "right", start: [1, 0], end: [1, 0] },
-          ]}
-        >
-          <Box gridArea="left">
-            <Box align="center" margin={{ vertical: "3vh" }}>
-              <Text textAlign="center" weight="bold" margin={{ bottom: "2vh" }}>
-                妥協せず話し合います
-              </Text>
-              <GiDiscussion size="150" />
-            </Box>
-            <Box>
-              <Paragraph margin={{ horizontal: "2vw" }}>
-                本当にいいプロダクトを作る上では、
-                <br />
-                1. アイデアのブラッシュアップ
-                <br />
-                2. ゴールの共有
-                <br />
-                の為のディスカッションが必須だと考えています。
-              </Paragraph>
-            </Box>
+    <>
+      { (size != 'small' && size != 'medium' ) && 
+        <Container background="lightblue">
+          <Box direction="row" justify="center" pad={{ top: "5vh" }}>
+            <StyledStar color="yellow" />
+            <Heading level="3" textAlign="center" margin="none">
+              STRENGTH{size}
+            </Heading>
           </Box>
-          <Box gridArea="right">
-            <Box align="center" margin={{ vertical: "3vh" }}>
-              <Text textAlign="center" weight="bold" margin={{ bottom: "2vh" }}>
-                技術力向上に拘ります
-              </Text>
-              <GiStairsGoal size="150" />
+        <Box alignSelf="center">
+          <Grid
+            rows={["medium"]}
+            columns={["medium", "medium"]}
+            gap="medium"
+            // @note ここの padding 指定は top だけでもいいかも
+            pad={{ vertical: "5vh" }}
+            areas={[
+              { name: "left", start: [0, 0], end: [0, 0] },
+              { name: "right", start: [1, 0], end: [1, 0] },
+            ]}
+          >
+            <Box gridArea="left">
+              <Box align="center" margin={{ vertical: "3vh" }}>
+                <Text textAlign="center" weight="bold" margin={{ bottom: "2vh" }}>
+                  妥協せず話し合います
+                </Text>
+                <GiDiscussion size="150" />
+              </Box>
+              <Box>
+                <Paragraph margin={{ horizontal: "2vw" }}>
+                  本当にいいプロダクトを作る上では、
+                  <br />
+                  1. アイデアのブラッシュアップ
+                  <br />
+                  2. ゴールの共有
+                  <br />
+                  の為のディスカッションが必須だと考えています。
+                </Paragraph>
+              </Box>
             </Box>
-            <Box>
-              <Paragraph margin={{ horizontal: "2vw" }}>
-                現在は Ruby, Railsを使った業務 がメインですが、 Go, React, Aws
-                など色々なものに興味があり、挑戦したいと思っています。
-              </Paragraph>
+            <Box gridArea="right">
+              <Box align="center" margin={{ vertical: "3vh" }}>
+                <Text textAlign="center" weight="bold" margin={{ bottom: "2vh" }}>
+                  技術力向上に拘ります
+                </Text>
+                <GiStairsGoal size="150" />
+              </Box>
+              <Box>
+                <Paragraph margin={{ horizontal: "2vw" }}>
+                  現在は Ruby, Railsを使った業務 がメインですが、 Go, React, Aws
+                  など色々なものに興味があり、挑戦したいと思っています。
+                </Paragraph>
+              </Box>
             </Box>
+          </Grid>
+        </Box>
+        </Container>
+      }
+      { (size == 'small' || size == 'medium' ) && 
+        <MobileContainer background="lightblue">
+          <Box direction="row" justify="center" pad={{ top: "5vh" }}>
+            <StyledStar color="yellow" />
+            <Heading level="3" textAlign="center" margin="none">
+              STRENGTH
+            </Heading>
           </Box>
-        </Grid>
-      </Box>
-    </Container>
+          <Box alignSelf="center">
+            {/* <Grid
+              rows={["medium"]}
+              columns={["medium", "medium"]}
+              gap="medium"
+              // @note ここの padding 指定は top だけでもいいかも
+              pad={{ vertical: "5vh" }}
+              areas={[
+                { name: "left", start: [0, 0], end: [0, 0] },
+                { name: "right", start: [1, 0], end: [1, 0] },
+              ]}
+            > */}
+              <Box gridArea="left">
+                <Box align="center" margin={{ vertical: "3vh" }}>
+                  <Text textAlign="center" weight="bold" margin={{ bottom: "2vh" }}>
+                    妥協せず話し合います
+                  </Text>
+                  <GiDiscussion size="150" />
+                </Box>
+                <Box pad={{horizontal: "3vw"}}>
+                  <Paragraph margin={{ horizontal: "2vw" }}>
+                    本当にいいプロダクトを作る上では、
+                    <br />
+                    1. アイデアのブラッシュアップ
+                    <br />
+                    2. ゴールの共有
+                    <br />
+                    の為のディスカッションが必須だと考えています。
+                  </Paragraph>
+                </Box>
+              </Box>
+              <Box gridArea="right">
+                <Box align="center" margin={{ vertical: "3vh" }}>
+                  <Text textAlign="center" weight="bold" margin={{ bottom: "2vh" }}>
+                    技術力向上に拘ります
+                  </Text>
+                  <GiStairsGoal size="150" />
+                </Box>
+                <Box pad={{horizontal: "3vw"}}>
+                  <Paragraph margin={{ horizontal: "2vw" }}>
+                    現在は Ruby, Railsを使った業務 がメインですが、 Go, React, Aws
+                    など色々なものに興味があり、挑戦したいと思っています。
+                  </Paragraph>
+                </Box>
+              </Box>
+            {/* </Grid> */}
+          </Box>
+        </MobileContainer>
+      }
+    </>
   );
 };
