1478745b3200407728e94ccf253aa71b60fd3bb8
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-05-25 16:47:15 +0900

    Updates

---
+++ precache-manifest.abc889df3d9e77789b14d4365924d2c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb654d9ab2bc3a70384d7065c31d82a",
+    "url": "/who-am-i/index.html"
+  },
+  {
+    "revision": "496d9b2da6cc3db8b05d",
+    "url": "/who-am-i/static/css/main.f0e1f1d0.chunk.css"
+  },
+  {
+    "revision": "ff18bb340c9801c620d7",
+    "url": "/who-am-i/static/js/2.481c0de2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/who-am-i/static/js/2.481c0de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496d9b2da6cc3db8b05d",
+    "url": "/who-am-i/static/js/main.d389cb5a.chunk.js"
+  },
+  {
+    "revision": "66a288a1b82ebdd33962",
+    "url": "/who-am-i/static/js/runtime-main.6bd32b09.js"
+  }
+]);
